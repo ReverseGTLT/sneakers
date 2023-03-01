763d5a37c7b8be9b1352e28b896ce2f6ea261bfb
ReverseGTLT <dzewrpifzc@gmail.com>
2023-03-01 20:33:47 +0400

    start

---
+++ src/futures/Main.js
@@ -0,0 +1,11 @@
+import Content from "./Content";
+import Slider from "./Slider";
+
+export default function Main() {
+    return (
+        <div className="main">
+            <Slider></Slider>
+            <Content></Content>
+        </div>
+    )
+}
